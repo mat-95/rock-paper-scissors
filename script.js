5152c9e03a5f3656e8ae7f65694d0dc19b897192
mat-95 <98151215+mat-95@users.noreply.github.com>
2025-02-27 15:06:53 +0200

    Plan function to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Hello World");
+
+/**
+ * Randomly selects the computer's choice for the game and returns it.
+ * @return {string} Randomly selected option (rock, paper, scissors).
+ */
+function getComputerChoice() {
+  // INIT a variable "choice" with a random number between 1 and 3 to represent the computer's choice
+  /*
+  CASE choice OF
+    1 : RETURN "rock"
+    2 : RETURN "paper"
+    3 : RETURN "scissors"
+  ENDCASE
+  */
+}
