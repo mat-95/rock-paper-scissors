649caf657270e41be3d5c624fc23d4541d0b623d
mat-95 <98151215+mat-95@users.noreply.github.com>
2025-02-27 15:27:33 +0200

    Plan function to get the user's choice

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /**
  * Randomly selects the computer's choice for the game and returns it.
- * @return {string} Randomly selected option (rock, paper, scissors).
+ * @returns {string} Randomly selected option (rock, paper, scissors).
  */
 function getComputerChoice() {
   // INIT a variable "choice" with a random number between 1 and 3 to represent the computer's choice
@@ -20,6 +20,18 @@ function getComputerChoice() {
     case 3:
       return "scissors";
   }
+}
+
+/**
+ * Prompts the user to make a choice for the game and returns it.
+ * @returns {string} The choice made by the user (rock, paper, scissors).
+ */
+function getHumanChoice() {
+  // INIT a variable "choice" to store the user's choice
+  // WHILE choice is not rock, paper or scissors
+  //  PROMPT user to enter a choice between rock, paper and scissors
+  // ENDWHILE
+  // RETURN choice  
 }
 
 console.log(getComputerChoice());
