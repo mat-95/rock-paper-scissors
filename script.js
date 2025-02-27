372e4d9e2c366522729140b7b694739a13fe4264
mat-95 <98151215+mat-95@users.noreply.github.com>
2025-02-27 15:15:51 +0200

    Implement function to get computer choice

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-console.log("Hello World");
-
 /**
  * Randomly selects the computer's choice for the game and returns it.
  * @return {string} Randomly selected option (rock, paper, scissors).
  */
 function getComputerChoice() {
   // INIT a variable "choice" with a random number between 1 and 3 to represent the computer's choice
+  let choice = Math.floor(Math.random() * 3) + 1;
   /*
   CASE choice OF
     1 : RETURN "rock"
@@ -13,4 +12,14 @@ function getComputerChoice() {
     3 : RETURN "scissors"
   ENDCASE
   */
+  switch (choice) {
+    case 1:
+      return "rock";
+    case 2:
+      return "paper";
+    case 3:
+      return "scissors";
+  }
 }
+
+console.log(getComputerChoice());
