94c300c0ec9fc44953fcd5ea4f9a4e7ffa1884c8
mat-95 <98151215+mat-95@users.noreply.github.com>
2025-02-28 11:26:06 +0200

    Implement function to play the whole game
     - Move playRound() inside playGame()
     - Call playRound() again if the result is a draw
     - Implement logic to play 5 rounds and display the winner at the end

--- script.js
+++ script.js
@@ -39,73 +39,86 @@ function getHumanChoice() {
   return choice;
 }
 
-/**
- * Takes the user's choice and the computer's choice and compares them, increments the winner's score and displays a winner announcement.
- * @param {string} humanChoice The user's choice for the round.
- * @param {string} computerChoice The computer's choice for the round.
- */
-function playRound(humanChoice, computerChoice) {
-  // INIT a variable "winner" to store the round's winner
-  let winner = "";
-  // IF humanChoice is the same as computerChoice THEN
-  if (humanChoice === computerChoice) {
-    //    SET winner to "none"
-    winner = "none";
-  }
-  // ELSE IF humanChoice is "rock" and computerChoice is "scissors", or humanChoice is "paper" and computerChoice is "rock", or humanChoice is "scissors" and computerChoice is "paper" THEN
-  else if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
-    //    SET winner to "human" 
-    winner = "human";
-  }
-  // ELSE
-  else {
-    //    SET winner to "computer"
-    winner = "computer";
-  }
-  // ENDIF
-  // DISPLAY the human's and the computer's choices
-  console.log(`You chose: ${humanChoice}. The computer chose: ${computerChoice}.`);
-  /*
-  CASE winner OF
-    "none" : PRINT "This is a draw!"
-    "human" : PRINT "Congratulations, you won!"
-              INCREMENT humanScore
-    "computer" : PRINT "Too bad, you lost!"
-                 INCREMENT computerScore
-  */
-  switch (winner) {
-    case "none":
-      console.log("This is a draw");
-      break;
-    case "human":
-      console.log("Congratulations, you won!");
-      humanScore++;
-      break;
-    case "computer":
-      console.log("Too bad, you lost!");
-      computerScore++;
-      break;
-  }
- // PRINT scores
- console.log(`Your score is: ${humanScore}. The computer's score is: ${computerScore}`);
-}
 
 function playGame() {
   // INIT humanScore variable to store the score of the user
   let humanScore = 0;
   // INIT computerScore variable to store the score of the computer
   let computerScore = 0;
+  /**
+   * Takes the user's choice and the computer's choice and compares them, increments the winner's score and displays a winner announcement.
+   * @param {string} humanChoice The user's choice for the round.
+   * @param {string} computerChoice The computer's choice for the round.
+   */
+  function playRound(humanChoice, computerChoice) {
+    // INIT a variable "winner" to store the round's winner
+    let winner = "";
+    // IF humanChoice is the same as computerChoice THEN
+    if (humanChoice === computerChoice) {
+      //    SET winner to "none"
+      winner = "none";
+    }
+    // ELSE IF humanChoice is "rock" and computerChoice is "scissors", or humanChoice is "paper" and computerChoice is "rock", or humanChoice is "scissors" and computerChoice is "paper" THEN
+    else if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
+      //    SET winner to "human" 
+      winner = "human";
+    }
+    // ELSE
+    else {
+      //    SET winner to "computer"
+      winner = "computer";
+    }
+    // ENDIF
+    // DISPLAY the human's and the computer's choices
+    console.log(`You chose: ${humanChoice}. The computer chose: ${computerChoice}.`);
+    /*
+    CASE winner OF
+      "none" : PRINT "This is a draw!"
+      "human" : PRINT "Congratulations, you won!"
+                INCREMENT humanScore
+      "computer" : PRINT "Too bad, you lost!"
+                   INCREMENT computerScore
+    */
+    switch (winner) {
+      case "none":
+        console.log("This is a draw");
+        // Play until there is a winner
+        playRound(getHumanChoice(), getComputerChoice());
+        break;
+      case "human":
+        console.log("Congratulations, you won!");
+        humanScore++;
+        break;
+      case "computer":
+        console.log("Too bad, you lost!");
+        computerScore++;
+        break;
+    }
+  }
   // INIT round variable to keep track of the current round
+  let round = 1;
   // WHILE round is less then or equal to 5
-  //    PRINT round
-  //    CALL function playRound() with getHumanChoice() and getComputerChoice() as arguments
+  while (round <= 5) {
+    //    PRINT round
+    console.log(`Round: ${round}`);
+    //    CALL function playRound() with getHumanChoice() and getComputerChoice() as arguments
+    playRound(getHumanChoice(), getComputerChoice());
+    // PRINT scores
+    console.log(`Your score is: ${humanScore}. The computer's score is: ${computerScore}`);  
+    round++;
+  }
   // ENDWHILE
-  // PRINT scores
   // IF humanScore is greater than computerScore
-  //    PRINT winning message
+  if (humanScore > computerScore) {
+    //    PRINT winning message
+    console.log("Congratulations, you won the game!");    
+  }
   // ELSE
-  //    PRINT loosing message
+  else {
+    //    PRINT loosing message
+    console.log("You lost! Better luck next time.");        
+  }
 }
 
-// Play a round of Rock Paper Scissors
-playRound(getHumanChoice(), getComputerChoice());
+// Play a full game of Rock Paper Scissors
+playGame();
